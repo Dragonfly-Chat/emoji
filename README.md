5107ec9709ec6b5c1a7982375fb26c7d91c74e51
Froggo <92762044+Froggo8311@users.noreply.github.com>
2022-05-02 21:18:23 +0000

    Initial commit. Added emojis and build script.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # emoji
 Emoji set for Dragonfly
+
+To quickly get started, run:
+```bash
+python build.py
+```
+This will create a `build` folder, which will contain all emojis.
+
+## Configuration
+The `emoji-build.json` file contains all of the building configuration.
+
+> `path` (`path`) - The path used for fetching emojis from the server.
+> `size` (`32px`) - The size of each emoji, in pixels.
---
+++ build.py
@@ -0,0 +1,74 @@
+VERSION = "0.0.1"
+
+import os, json
+
+config = {
+  "path": "/emoji/",
+  "size": "32px"
+}
+
+if os.path.exists("emoji-build.json") and os.path.isfile("emoji-build.json"):
+  config_in_file = open("emoji-build.json")
+  config_in = json.loads(config_in_file.read())
+  config_in_file.close()
+  for c in config_in.keys():
+    config[c] = config_in[c]
+
+if os.path.exists("build"):
+  print("Build directory exists, clearing build...")
+  for file in os.listdir("build/emoji"):
+    os.remove(f"build/emoji/{file}")
+  for file in os.listdir("build"):
+    if os.path.isfile(file):
+      os.remove(f"build/{file}")
+else:
+  print("Creating build directories...")
+  os.mkdir("build")
+  os.mkdir("build/emoji")
+print("Done.")
+
+print("Building JavaScript library...")
+js = """\
+/* Dragonfly emoji set v%ver%. Licensed under MIT. All credit goes to contributors. */ 
+$_emoji={
+  p:'%path%',
+  s:'%size%',
+  v:'%ver%',
+  u:function(e=""){
+    return this.p+e.split(':')[1];
+  },
+  c:function(e=""){
+    return '<img src="'+this.u(e)+'" width="'+this.s+'" height="'+this.s+'"/>';
+  },
+  text:function(t=""){
+    var el=t.match(/:(.*?):/g);
+    var rl=[];
+    el.forEach((c)=>{
+      if(!rl.includes(c)){
+        rl.push(c);
+      }
+    });
+    rl.forEach((e)=>{
+      t=t.replace(e,this.c(e));
+    });
+    return t;
+  }
+}
+"""
+
+# Minify, build, & save JavaScript file
+js_file = open(f"build/dragonfly-emoji-{VERSION}.min.js", 'w')
+js_file.write(js.replace('\n','').replace('  ','').replace('%path%', config['path']).replace('%size%', config['size']).replace('%ver%', VERSION))
+js_file.close()
+print("Done.")
+
+print("Building emojis...")
+for category in os.listdir("assets/other"):
+  for file in os.listdir(f"assets/other/{category}"):
+    infile = open(f"assets/other/{category}/{file}", 'rb')
+    outfile = open(f"build/emoji/{category}_{file.split('.')[0]}", 'wb')
+    outfile.write(infile.read())
+    infile.close()
+    outfile.close()
+
+print("Done.")
